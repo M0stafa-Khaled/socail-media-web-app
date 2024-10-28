e8c187e7a136682724dbc9788e741d5e421e6f51
Mostafa <mostafa6320005@gmail.com>
2024-10-28 17:40:29 +0300

    fix error

--- src/components/shared/GridPostsList.tsx
+++ src/components/shared/GridPostsList.tsx
@@ -15,9 +15,10 @@ const GridPostsList = ({
   showStats = true,
 }: IProps) => {
   const { user } = useUserContext();
+  console.log(posts)
   return (
     <ul className="grid-container">
-      {posts.map((post) => (
+      {posts.length > 0 && posts.map((post) => (
         <li key={post.$id} className="relative min-w-80 h-80">
           <Link to={`/posts/${post.$id}`} className="grid-post_link">
             <img
@@ -32,16 +33,16 @@ const GridPostsList = ({
               <div className="flex items-center justify-start gap-2 flex-1">
                 <img
                   src={
-                    post.creator.imageUrl ||
+                    post?.creator?.imageUrl ||
                     "/assets/icons/profile-placeholder.svg"
                   }
                   alt="creator"
                   className="w-8 h-8 rounded-full"
                 />
-                <p className="line-clamp-1">{post.creator.name}</p>
+                <p className="line-clamp-1">{post?.creator?.name}</p>
               </div>
             )}
-            {showStats && <PostStats post={post} userId={user.id ?? ""} />}
+            {showStats && <PostStats post={post} userId={user?.id ?? ""} />}
           </div>
         </li>
       ))}
